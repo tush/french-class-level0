f2ad0dfbfbaedeaaa774500a1fc03f19794b404d
Tushar Wadekar <tusharwadekar@Tushars-MacBook-Pro.local>
2020-03-01 23:38:15 +0100

    first commit

--- data.js
+++ data.js
@@ -1,15 +1,245 @@
-const dataList = [
+const whoAreYou = [
     {
         question: {
             french: "Comment t'appelles-tu?",
             english: "What's your name?",
-            pronunciation: "को मे ता पेल तू ?"
+            pronunciation: "को मे ता पेल तू?"
         },
         answer: {
             french: "Je m'appelle Tushar",
             english: "My name is Tushar",
-            pronunciation: "को मे ता पेल तू ?"
+            pronunciation: "जे मा पेल तुषार"
+        }
+    },
+    {
+        question: {
+            french: "Tu as quel age?",
+            english: "How old are you?",
+            pronunciation: "तू आ क्वे आज?"
+        },
+        answer: {
+            french: "J'age 40",
+            english: "My age is 40 (quarante)",
+            pronunciation: "जे आज क्वेरान्त"
+        }
+    },
+    {
+        question: {
+            french: "D'ou viens-tu?",
+            english: "Where do you come from?",
+            pronunciation: "दु वि आ तू?"
+        },
+        answer: {
+            french: "Je viens d'Indie",
+            english: "I am from India",
+            pronunciation: "जे जान दान or जे वि दे इंडी"
+        }
+    },
+    {
+        question: {
+            french: "Quelle est ta nationalite?",
+            english: "What is your nationality?",
+            pronunciation: "के ले ता नॅशनॅलित?"
+        },
+        answer: {
+            french: "Je suis Indian",
+            english: "I am Indian",
+            pronunciation: "जे स्विस आनदीआन"
+        }
+    },
+    {
+        question: {
+            french: "Quelle est ta profession?",
+            english: "What is your profession?",
+            pronunciation: "के ले ता प्रॉफेसीओं?"
+        },
+        answer: {
+            french: "Je travaille en informatique",
+            english: "I work in IT",
+            pronunciation: "जे थ्राव्याऑन इन्फॉर्मटिक"
+        }
+    },
+    {
+        question: {
+            french: "Tu habites ou?",
+            english: "Where do you live?",
+            pronunciation: "तू आबी तू?"
+        },
+        answer: {
+            french: "Je habite'A Etterbeek",
+            english: "I live in Etterbeek",
+            pronunciation: "जे आबी ता इत्तरबीक"
         }
     },
 ]
-window.dataList = dataList;
+window.whoAreYou = whoAreYou;
+
+
+
+
+
+const howAreYou = [
+    {
+        question: {
+            french: "Bonjour madame",
+            english: "Hello Madam",
+            pronunciation: "बोनजोर मादाम"
+        },
+        answer: {
+            french: "Bonjour monsieur",
+            english: "Hello Sir",
+            pronunciation: "बोनजोर मिसयु"
+        }
+    },
+    {
+        question: {
+            french: "Vous allez bien",
+            english: "Your are fine?",
+            pronunciation: "वू जा ली बिया?"
+        },
+        answer: {
+            french: "Tres bin, merci!",
+            english: "Very well, Thank you!",
+            pronunciation: "थ्रेसबीआ मेर्सि"
+        }
+    },
+    {
+        question: {
+            french: "Salut Manon! Tu vas bien?",
+            english: "Hi Manon! You're okay?",
+            pronunciation: "सेलू मेनन तू वा बिया?"
+        },
+        answer: {
+            french: "Salut Alice! Oui, ca va, et toi?",
+            english: "Hi Alice! Yes I'm fine and you?",
+            pronunciation: "सेलू आलीस! वि सावा. ए तू आ?"
+        }
+    },
+
+    {
+        question: {
+            french: "Bonne soiree, au revoir!",
+            english: "Good evening, goodbye!",
+            pronunciation: "बोन स्वारी! ऊ रूरवा"
+        },
+        answer: {
+            french: "Au revoir monsieur, a bientot",
+            english: "goodbye sir, see you soon",
+            pronunciation: "ऊरूरवा मिसयु, आबिअंतो"
+        }
+    },
+    {
+        question: {
+            french: "je serre la main ou je fais la bise?",
+            english: "I shake my hand or kiss?",
+            pronunciation: "जे सेरला माऊंज फेलाबीझ?"
+        },
+        answer: {
+            french: "",
+            english: "",
+            pronunciation: ""
+        }
+    },
+    {
+        question: {
+            french: "je parle",
+            english: "I speak",
+            pronunciation: "जे पार्ल?"
+        },
+        answer: {
+            french: "je parle english, hindi et marathi",
+            english: "i speak english, hindi and marathi",
+            pronunciation: "जे पार्ल इंग्लिश हिंदी ए मराठी "
+        }
+    },
+]
+window.howAreYou = howAreYou;
+
+const classCommunication = [
+    {
+        question: {
+            french: "Je comprends",
+            english: "I understand",
+            pronunciation: "जे कुंपरान"
+        },
+        answer: {
+            french: "je ne comprends pas",
+            english: "I do not understand",
+            pronunciation: "जे ने कुंपरान पास"
+        }
+    },
+    {
+        question: {
+            french: "qu est-ce que ça veut dire?",
+            english: "What does it mean",
+            pronunciation: "क़ु एस्का सा विडिअर"
+        },
+        answer: {
+            french: "comment dire en francis?",
+            english: "how to say in francis?",
+            pronunciation: "कोमोन्ट डेरा फ्रान्सिस?"
+        }
+    },
+    {
+        question: {
+            french: "je propose",
+            english: "I propose",
+            pronunciation: "जे प्रपोस"
+        },
+        answer: {
+            french: "très bien",
+            english: "very well",
+            pronunciation: "ट्रेबिया"
+        }
+    },
+    {
+        question: {
+            french: "je ne suis pas d'accord",
+            english: "I do not agree",
+            pronunciation: "जे ने स्वी पॅड ऑकर"
+        },
+        answer: {
+            french: "qu'est-ce que ç'est?",
+            english: "What is that?",
+            pronunciation: "क्वेस्ट का से?"
+        }
+    },
+    {
+        question: {
+            french: "pouvez-vous repeter, s'il vous plait?",
+            english: "can you repeat please?",
+            pronunciation: "पुवे व्रुरेपीटर, सिल्वउपले?"
+        },
+        answer: {
+            french: "je suis d'accord",
+            english: "I agree",
+            pronunciation: "जे स्वी डेकोर"
+        }
+    },
+    {
+        question: {
+            french: "écoutez",
+            english: "Listen",
+            pronunciation: "एकुटे"
+        },
+        answer: {
+            french: "lisez",
+            english: "read",
+            pronunciation: "लिझे"
+        }
+    },
+    {
+        question: {
+            french: "répétez",
+            english: "repeat",
+            pronunciation: "रिपीटे"
+        },
+        answer: {
+            french: "écrivez",
+            english: "write",
+            pronunciation: "एक्रिवे"
+        }
+    },
+]
+window.classCommunication = classCommunication;
+
--- main-vue.js
+++ main-vue.js
@@ -1,11 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
-    dataList: window.dataList,
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
+    whoAreYou: window.whoAreYou,
+    howAreYou: window.howAreYou,
+    classCommunication: window.classCommunication,
   }
 })
